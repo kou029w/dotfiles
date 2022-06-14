7d592c85eddbd7635eabf0f8d344b93df0b34a01
Kohei Watanabe <kou029w@gmail.com>
2022-06-15 00:15:50 +0900

    create termux addon

--- mod.bash
+++ mod.bash
@@ -1,16 +1,3 @@
-function install_apt_packages() {
-  local packages
-  local apt_sources_dir
-  . packages.bash
-  eval "$(apt-config shell apt_sources_dir 'Dir::Etc::sourceparts/d')"
-  rsync -rv etc/apt/keyrings/ /usr/share/keyrings/
-  rsync -rv etc/apt/sources.list.d/ "$apt_sources_dir"
-  [[ -d etc/default ]] \
-    && rsync -rv etc/default/ /etc/default/
-  apt update
-  apt install -y "${packages[@]}"
-}
-
 function install_dotfiles() {
   local home
   local dotfile
@@ -26,6 +13,19 @@ function install_dotfiles() {
   done
 }
 
+function install_apt_packages() {
+  local packages
+  local apt_sources_dir
+  . packages.bash
+  eval "$(apt-config shell apt_sources_dir 'Dir::Etc::sourceparts/d')"
+  rsync -rv etc/apt/keyrings/ /usr/share/keyrings/
+  rsync -rv etc/apt/sources.list.d/ "$apt_sources_dir"
+  [[ -d etc/default ]] \
+    && rsync -rv etc/default/ /etc/default/
+  apt update
+  apt install -y "${packages[@]}"
+}
+
 function install_addons() {
   local addon
   for addon in "$@"; do
@@ -34,17 +34,4 @@ function install_addons() {
       ./install
     )
   done
-}
-
-function install_gsettings() {
-  local uid
-  local gsettings_file
-  local gsettings_dir
-  uid=${SUDO_UID:-$(id -u)}
-  gsettings_file=$1
-  gsettings_dir=$2
-  < "$gsettings_file" \
-    sudo --user="#$uid" \
-    DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$uid/bus" \
-    dconf load "$gsettings_dir"
 }
---
+++ termux/packages.bash
@@ -0,0 +1,12 @@
+packages=(
+dnsutils
+gh
+git
+iproute2
+jq
+mdbook
+nodejs-lts
+openssh
+rclone
+sqlite
+)
