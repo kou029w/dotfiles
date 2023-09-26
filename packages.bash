0003d42594edc3430620a8d0af4c031124a6f37a
Kohei Watanabe <nebel@fogtype.com>
2023-09-27 00:54:31 +0900

    add git-crypt

--- packages.bash
+++ packages.bash
@@ -1,18 +1,19 @@
 packages=(
+build-essential
 cloudflared
+curl
 docker-ce
 docker-compose-plugin
-nodejs
-tailscale
-build-essential
-curl
 fdupes
 ffmpeg
+git-crypt
 imagemagick
 knot-dnsutils
+nodejs
 qemu-user-static
 screen
 smartmontools
+tailscale
 )
 
 if [[ "$(df --output=fstype . | sed 1d)" == "btrfs" ]]; then
