d358817ac0830b2c78efaa72087a3da48e21e3b9
Kohei Watanabe <nebel@fogtype.com>
2025-11-22 13:47:23 +0900

    refactor

--- desktop/packages.bash
+++ desktop/packages.bash
@@ -1,15 +1,15 @@
 packages=(
-# main
+	## main repository
 	deja-dup
 
-# universe
+	## universe repository
 	asunder
 	libpam-u2f
 	mozc-utils-gui
 	timeshift
 	xsel
 
-# 3rd-party
+	## 3rd-party repository
 	code
 	google-chrome-stable
 	microsoft-edge-stable
--- packages.bash
+++ packages.bash
@@ -1,5 +1,5 @@
 packages=(
-# main
+	## main repository
 	bash-completion
 	build-essential
 	curl
@@ -8,11 +8,11 @@ packages=(
 	smartmontools
 	sqlite3
 
-# universe
+	## universe repository
 	ffmpeg
 	git-crypt
 
-# 3rd-party
+	## 3rd-party repository
 	docker-ce
 	docker-compose-plugin
 	mise
